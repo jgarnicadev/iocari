63a01e5c2364ffcb05a8b9de8937ca0f263e1e24
afc-legio <35813732+afc-legio@users.noreply.github.com>
2019-04-05 17:32:33 +0200

    retoques presentacion carruselpartida

--- components/CarruselPartida.js
+++ components/CarruselPartida.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { Card, Title } from 'react-native-paper';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Title, IconButton } from 'react-native-paper';
 
 class CarruselPartida extends React.Component {
     render() {
       return (
         <Card style={styles.container} elevation={5}>
-            {this.props.partida.image != '' &&
-              <Card.Cover source={{uri: this.props.partida.image}} /> }
+            <Card.Cover source={{uri: this.props.partida.image}} />
             <Card.Content>
               <Title>{this.props.partida.nombre}</Title>
               <Text>Fecha: {this.props.partida.fecha} {this.props.partida.hora}</Text>
-              <Text>Jugadores: {this.props.partida.players}</Text>
             </Card.Content>
+            <View style={styles.txtJugadores}>
+              <IconButton icon="wc" color="white" size={20} style={{ margin:0, padding: 0 }}></IconButton>
+              <Text style={{ color: 'white' }}>{this.props.partida.jugadores_apuntados} / {this.props.partida.players}</Text>
+            </View>
         </Card>
       );
     }
@@ -25,6 +27,15 @@ const styles = StyleSheet.create({
       marginLeft:10,
       marginTop:5,
       width:250,
+      paddingBottom:5,
+    },
+    txtJugadores: {
+      position:"absolute",
+      left:10,
+      top:155,
+      backgroundColor:"rgba(0, 0, 0, 0.5)",
+      flexDirection:'row',
+      alignItems:'center',
     },
 });
 
