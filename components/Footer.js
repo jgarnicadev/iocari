75573bbfd7d8ab9f7af4fe97dd507803adf01ba9
afc-legio <35813732+afc-legio@users.noreply.github.com>
2019-04-05 13:04:17 +0200

    retoques estilo footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import { IconButton } from 'react-native-paper';
 
 class Footer extends React.Component {
   render() {
     return (
         <View style={styles.container}>
           <View style={styles.btnCrearPartida}>
-            <Button title="Crear partida" onPress={() => this.props.navigation.navigate('crearPartida')}/>
+            <IconButton
+              size={150}
+              icon={({ size }) => (
+                <Image
+                  source={require('../assets/btnCrearPartida.png')}
+                  style={{ width: size, height: size }}
+                />
+              )}
+              onPress={() => this.props.navigation.navigate('crearPartida')}
+            />
           </View>
           <View style={styles.footer}>
-            <View style={styles.boton}><Text>Home</Text></View>
-            <View style={styles.boton}><Text>Biblioteca</Text></View>
-            <View style={styles.boton}><Text>Perfil</Text></View>
-            <View style={styles.boton}><Text>Alertas</Text></View>
+            <View style={styles.boton}>
+              <IconButton icon="home"></IconButton>
+              <Text>Home</Text>
+            </View>
+            <View style={styles.boton}>
+              <IconButton icon="view-list" color="#bbb"></IconButton>
+              <Text style={{ color: '#bbb'}}>Biblioteca</Text>
+            </View>
+            <View style={styles.boton}>
+              <IconButton icon="person" color="#bbb"></IconButton>
+              <Text style={{ color: '#bbb'}}>Perfil</Text>
+            </View>
+            <View style={styles.boton}>
+              <IconButton icon="notifications" color="#bbb"></IconButton>
+              <Text style={{ color: '#bbb'}}>Alertas</Text>
+            </View>
           </View>
         </View>
     );
@@ -30,10 +52,9 @@ const styles = StyleSheet.create({
     },
     btnCrearPartida: {
       paddingHorizontal:15,
-      marginVertical:10,
+      alignItems:"center",
     },
     boton: {
-      paddingTop:50,
       paddingHorizontal:15,
       paddingBottom:20,
       alignItems:'center',
