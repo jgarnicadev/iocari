e0e6e927371490c91f9cf0ff7637361368bdeb57
afc-legio <35813732+afc-legio@users.noreply.github.com>
2019-04-10 17:05:06 +0200

    retoques presentacion pantalla partida

--- components/Partida.js
+++ components/Partida.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, AsyncStorage, ActivityIndicator, Image, Text } from 'react-native';
-import { Button } from 'react-native-paper';
+import { StyleSheet, View, AsyncStorage, ActivityIndicator, Image, Text, ImageBackground } from 'react-native';
+import { Button, IconButton } from 'react-native-paper';
 
 import Header from './Header';
 
@@ -60,33 +60,50 @@ class Partida extends React.Component {
         return (
             <View style={styles.container}>
               <Header title={this.state.partida.nombre} />
-              <View style={styles.cabeceraPartida}>
-                <Image source={{ uri: this.state.partida.image }} style={{ width:'100%', height:200}} />
-                <Text>{this.state.partida.fecha}</Text>
-                <Text>{this.state.partida.hora}</Text>
-                <Text>{this.state.partida.duracion}</Text>
-                <Text>{this.state.partida.jugadores_apuntados} / {this.state.partida.players}</Text>
+              <ImageBackground style={styles.cabeceraPartida} source={{ uri: this.state.partida.image }} imageStyle={{ resizeMode: 'cover', opacity:0.3 }} >
+                <View style={styles.cabeceraWarpTxt}>
+                  <IconButton icon={require('../assets/ico-fecha.png')} color="white" size={20} style={{ margin:0, padding: 0 }}></IconButton>
+                  <Text style={[styles.txtBlanco, styles.txtCabecera]}>{this.state.partida.fecha}</Text>
+                </View>
+                <View style={styles.cabeceraWarpTxt}>
+                  <IconButton icon={require('../assets/ico-hora.png')} color="white" size={20} style={{ margin:0, padding: 0 }}></IconButton>
+                  <Text style={[styles.txtBlanco, styles.txtCabecera]}>{this.state.partida.hora}</Text>
+                </View>
+                <View style={styles.cabeceraWarpTxt}>
+                  <IconButton icon={require('../assets/ico-duracion.png')} color="white" size={20} style={{ margin:0, padding: 0 }}></IconButton>
+                  <Text style={[styles.txtBlanco, styles.txtCabecera]}>{this.state.partida.duracion}</Text>
+                </View>
+                <View style={styles.txtJugadores}>
+                  <IconButton icon="wc" color="white" size={20} style={{ margin:0, padding: 0 }}></IconButton>
+                  <Text style={styles.txtBlanco}>{this.state.partida.jugadores_apuntados} / {this.state.partida.players}</Text>
+                </View>
+              </ImageBackground>
+              <View style={styles.contenedor}>
+                <View style={[styles.cabeceraWarpTxt, { marginBottom:10 }]}>
+                  <IconButton icon="view-list" color="#7C7C7C" size={20} style={{ margin:0, padding: 0 }}></IconButton>
+                  <Text style={[styles.txtGris, styles.txtTitulo]}>Juegos para la sesión</Text>
+                </View>
+                <Text style={styles.txtGris}>{this.state.partida.juegos}</Text>
               </View>
-              <View style={styles.juegos}>
-                <Text>Juegos para la sesión</Text>
-                <Text>{this.state.partida.juegos}</Text>
+              <View style={[styles.contenedor, styles.bordeContenedor]}>
+                <Text style={styles.txtGris}>{this.state.partida.descripcion}</Text>
               </View>
-              <View style={styles.descripcion}>
-                <Text>{this.state.partida.descripcion}</Text>
-              </View>
-              <View style={styles.personasApuntadas}>
-                <Text>Personas apuntadas</Text>
+              <View style={styles.contenedor}>
+                <Text style={[styles.txtGris, styles.txtTitulo, { marginBottom:10 }]}>Personas apuntadas</Text>
                 {this.state.partida.jugadores.map((elem) => 
-                  <Text key={elem.id}>{elem.player_name}</Text>
+                  <Text style={styles.txtGris} key={elem.id}>{elem.player_name}</Text>
                 )}
               </View>
-              <View style={styles.lugar}>
-                  <Text>{this.state.partida.lugar}</Text>
+              <ImageBackground style={styles.contenedorLugar} source={require('../assets/mapa.jpg')} imageStyle={{ resizeMode: 'cover', opacity:0.3 }} >
+                  <Text style={[styles.txtBlanco, styles.txtCabecera]}>{this.state.partida.lugar}</Text>
+                  <IconButton icon="place" color="white" size={20} style={styles.markerLugar}></IconButton>
+              </ImageBackground>
+              <View style={styles.contenedor}>
+                  <Text style={[styles.txtGris, styles.txtTitulo, { marginBottom:10 }]}>Comentarios</Text>
               </View>
-              <View style={styles.comentarios}>
-                  <Text>Comentarios</Text>
+              <View style={[styles.contenedor, { display:'none'}]}>
+                <Button style={styles.button} mode="contained" dark="true" color="#f50057" onPress={this.apuntarse.bind(this)}>Apuntarse</Button>
               </View>
-              <Button style={styles.btnApuntarse} mode="contained" dark="true" color="#f50057" onPress={this.apuntarse.bind(this)}>Apuntarse</Button>
             </View>
         );
     }
@@ -104,7 +121,63 @@ const styles = StyleSheet.create({
       justifyContent:'flex-start',
       backgroundColor:'#f3f1f1',
     },
-});
+    cabeceraPartida: {
+      backgroundColor:'#0277bd',
+      paddingHorizontal:20,
+      paddingBottom:10,
+      paddingTop:40,
+    },
+    txtBlanco: {
+      color: 'white',
+    },
+    txtJugadores: {
+      position:"absolute",
+      right:20,
+      bottom:10,
+      backgroundColor:"rgba(0, 0, 0, 0.5)",
+      flexDirection:'row',
+      alignItems:'center',
+      paddingRight:10,
+    },
+    txtCabecera: {
+      paddingVertical:10,
+      paddingLeft:5,
+    },
+    contenedor: {
+      paddingVertical:20,
+      paddingHorizontal:20,
+    },
+    button: {
+      paddingVertical:10,
+    },
+    txtGris: {
+      color:'#7C7C7C',
+    },
+    txtTitulo: {
+      fontSize:15,
+    },
+    bordeContenedor: {
+      borderWidth: 1,
+      borderColor: '#7C7C7C',
+    },
+    contenedorLugar: {
+      backgroundColor:'#0277bd',
+      paddingHorizontal:20,
+      paddingBottom:10,
+      paddingTop:10,
+    },
+    cabeceraWarpTxt: {
+      flexDirection:'row',
+      alignItems:'center',
+    },
+    markerLugar: {
+      margin:0,
+      padding:0,
+      position:'absolute',
+      top:10,
+      right:20,
+    },
+  });
 
 export default Partida;
 
